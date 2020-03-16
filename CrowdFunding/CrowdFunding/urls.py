8141c1240a8c64f85e65cd9422c129095ac3c434
minariaad <mina.riaad@gmail.com>
2020-03-16 20:16:15 +0200

    profile

--- CrowdFunding/CrowdFunding/urls.py
+++ CrowdFunding/CrowdFunding/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Profile import views as profile_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/profile/<int:id>',profile_views.profile)
 ]
--- CrowdFunding/Profile/views.py
+++ CrowdFunding/Profile/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from Authentication.models import Users
 
 # Create your views here.
+
+#profile page
+def profile(request,id):
+    obj=Users.objects.get(user_id=id)
+    context={
+        "data":obj
+    }
+    print(context)
+    return render(request,"profile/user_profile.html",context)
