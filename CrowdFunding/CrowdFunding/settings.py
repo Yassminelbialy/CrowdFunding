bb319c5b15f1bf960aba98e3cb1744098e152a8e
yassminaElbialy <monyelbialy@gmail.com>
2020-03-16 06:13:35 -0700

    msg

--- CrowdFunding/CrowdFunding/settings.py
+++ CrowdFunding/CrowdFunding/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'projects',
-    'donation',
+    'donation'
 ]
 
 MIDDLEWARE = [
--- CrowdFunding/users/models.py
+++ CrowdFunding/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    id=models.AutoField(primary_key=True)
+    user_id=models.AutoField(primary_key=True)
     First_name=models.CharField(max_length=100)
     Last_name=models.CharField(max_length=100)
     password=models.BigIntegerField()
