1386c2e4a74edff20d2decb256c2da6629b83741
salah elhossiny <salah_elhossiny856@yahoo.com>
2020-03-21 23:37:32 +0200

    update project

--- CrowdFunding/CrowdFunding/urls.py
+++ CrowdFunding/CrowdFunding/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Profile import views as profile_views
-from Project_Creation.views import create_project, show_project
+
+from Project_Creation.views import create_project, show_project,donate_project, comment_project, delete_project
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +26,8 @@ urlpatterns = [
     path('user/profile/edit',profile_views.edit_profile), 
     path('user/profile/addProject', create_project, name = "create_project"),  
     path('user/profile/projects/<int:id>', show_project, name = "show_project"),
+    path('project/donate/<int:id>', donate_project, name="project_donate"),
+    path('project/comment/<int:id>', comment_project, name="project_comment"),
+    path('project/delete/<int:id>', delete_project, name="project_delete")
 ]
 #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
