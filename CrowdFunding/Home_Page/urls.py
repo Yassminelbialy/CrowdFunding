b02048de426d4adf7f168c983b120613f2596421
MahmoudEzz <mahmoud.ezz49@gmail.com>
2020-03-23 12:18:26 +0200

    add search

--- CrowdFunding/Home_Page/urls.py
+++ CrowdFunding/Home_Page/urls.py
@@ -5,4 +5,5 @@ app_name = 'Home_Page'
 urlpatterns = [
     path('', views.index, name='index'),
     path('category/<int:cat_id>',views.select_tag, name='category'),
+	path('search/', views.search, name='search'),
 ]
