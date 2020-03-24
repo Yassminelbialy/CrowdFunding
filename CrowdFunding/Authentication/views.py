564d28e53970f0db60f3635bf15396bc92d42dc6
minariaad <mina.riaad@gmail.com>
2020-03-24 19:46:47 +0200

    meeting

--- CrowdFunding/Authentication/views.py
+++ CrowdFunding/Authentication/views.py
@@ -18,6 +18,7 @@ from django.contrib.auth import authenticate , login ,logout
 from django.http import HttpResponseRedirect , HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+import uuid
 
 def register(request):
     registered = False
